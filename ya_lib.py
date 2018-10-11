b2cf88b31f928d1949e0c2fbc196ceda1aae4ec4
sergej-i <44048595+sergej-i@users.noreply.github.com>
2018-10-11 11:05:41 +0300

    clearing

--- ya_lib.py
+++ ya_lib.py
@@ -1,7 +1,7 @@
 from random import randint
 
 max_diff_items = 100 # пусть имён будет конечное небольшое число
-max_items_count = 50 # ограничим максимальное число генерируемых словарей
+max_items_count = 20 # ограничим максимальное число генерируемых словарей (max_items_count <= max_diff_items)
 max_item_val = 1000000 # пусть значения элементов в словарях также ограничены сверху
 
 zeros = len(str(max_diff_items)) # чтобы имена были красивые...
@@ -9,5 +9,9 @@ zeros = len(str(max_diff_items)) # чтобы имена были красивы
 def gen_name_by_i(i):
     return 'key_' + f'{randint(0, i + 1):0>{zeros}}'
 
+def gen_name():
+    return 'key_' + f'{randint(1, max_diff_items):0>{zeros}}'
+
 def gen_val():
     return randint(1, max_item_val)
+
